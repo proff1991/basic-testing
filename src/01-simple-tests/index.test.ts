11b616c645d7367d019074bc2406535254230b7b
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2026-04-18 19:09:59 +0300

    feat: implement 01-simple-tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,32 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { describe, expect, test } from '@jest/globals';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 345, b: 678, action: Action.Add })).toBe(1023);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 987, b: 432, action: Action.Subtract })).toBe(555);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 123, b: 456, action: Action.Multiply })).toBe(56088);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 864, b: 108, action: Action.Divide })).toBe(8);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 11, b: 3, action: Action.Exponentiate })).toBe(1331);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 345, b: 678, action: '%' })).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: '345', b: 678, action: Action.Add })).toBeNull();
   });
 });
